ecd633b0ab91fa8e5d90fd76e394a164936e53d4
tdroso <theano_droso@yahoo.com>
2025-05-11 20:00:05 +0300

    mapping for issued date

---
+++ src/main/java/run_extra_mapping.java
@@ -0,0 +1,77 @@
+import org.eclipse.rdf4j.model.*;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.util.Values;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
+import org.eclipse.rdf4j.model.vocabulary.XSD;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+
+import java.io.*;
+import java.util.UUID;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class run_extra_mapping {
+
+    public static void main(String[] args) {
+        String csvFile = "./src/main/resources/bibo/books.csv";
+        String baseURI = "http://example.org/";
+        Model model = new LinkedHashModel();
+
+        ValueFactory vf = SimpleValueFactory.getInstance();
+        SimpleDateFormat inputFormat = new SimpleDateFormat("M/d/yyyy");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String header = br.readLine(); // Skip header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",", -1);
+                if (fields.length < 12) continue;
+
+                String bookID = fields[0].trim();
+                String authorsRaw = fields[2].trim();
+                String pubDateRaw = fields[10].trim();
+
+                // Create Book IRI using bookID
+                IRI bookIRI = vf.createIRI(baseURI, "book/" + bookID);
+                model.add(bookIRI, RDF.TYPE, vf.createIRI("http://purl.org/ontology/bibo/Book"));
+
+                // Process authors
+                for (String name : authorsRaw.split("/")) {
+                    name = name.trim();
+                    if (name.isEmpty()) continue;
+
+                    String personId = name.toLowerCase().replaceAll("[^a-z0-9]", "_");
+                    IRI personIRI = vf.createIRI(baseURI, "person/" + personId);
+                    model.add(personIRI, RDF.TYPE, vf.createIRI("http://xmlns.com/foaf/0.1/Person"));
+                    model.add(personIRI, vf.createIRI("http://xmlns.com/foaf/0.1/name"), vf.createLiteral(name));
+                    model.add(bookIRI, vf.createIRI("http://purl.org/dc/terms/creator"), personIRI);
+                }
+
+                // Convert and add publication date
+                try {
+                    Date parsedDate = inputFormat.parse(pubDateRaw);
+                    String isoDate = outputFormat.format(parsedDate);
+                    Literal dateLiteral = vf.createLiteral(isoDate, XSD.DATETIME);
+                    model.add(bookIRI, DCTERMS.ISSUED, dateLiteral);
+                } catch (ParseException e) {
+                    System.err.println("Failed to parse date: " + pubDateRaw);
+                }
+            }
+
+            // Write output RDF to TTL file
+            try (OutputStream out = new FileOutputStream("./src/main/resources/bibo/results_2.ttl")) {
+                Rio.write(model, out, RDFFormat.TURTLE);
+            }
+
+            System.out.println("RDF mapping complete: books_output.ttl");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
